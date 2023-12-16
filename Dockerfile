4b47c04bff3d83186490878aff477241fff73d57
Amit Kumar <amitrke@gmail.com>
2023-12-16 09:05:03 -0800

    Add GitHub Copilot plugins and update Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,8 @@ FROM python:latest
 RUN apt update
 RUN apt install git -y
 WORKDIR /app
-
+COPY requirements.txt /app
 RUN python -m pip install --upgrade pip
+RUN pip install -r requirements.txt
 
 CMD tail -f /dev/null
--- src/vzw.py
+++ src/vzw.py
@@ -11,83 +11,92 @@ print(sys.argv[1])
 from pdfminer.high_level import extract_pages
 from pdfminer.layout import LTTextContainer, LTChar, LTAnno, LTPage
 
-data = {}
+dataDict = {
+    "currentContext": None
+}
 
-skipPageText = [
-    "Questions about your bill?",
-    "Talk activity",
-    "Talk activity (cont.)",
-    "Customer Proprietary Network Information",
-    "Additional information continued"
-]
-
-fields = {
-    "billingPeriod": {
-        "label": "Billing period",
-        "page": 0,
-        # "xy": {
-        #     "x": 0,
-        #     "y": 0
-        # }
+def parseChargesByLineSummary(textLine: str, x: float, y: float, height: float):
+    if x > 400:
+        return
+    if "chargesByLineSummary" not in dataDict:
+        dataDict["chargesByLineSummary"] = {}
+    
+    print("TextLine=" + textLine +",Height=" + str(height) + ",x=" + str(x) + ",y=" + str(y))
+    if "Account" in textLine:
+        dataDict["accountNumber"] = textLine.split(":")[1].strip()
+    elif "Billing period" in textLine:
+        dataDict["billingPeriod"] = textLine.split(":")[1].strip()
+    elif "Invoice" in textLine:
+        dataDict["invoiceNumber"] = textLine.split(":")[1].strip()
+    elif height == 10.0 and x == 39.6:
+        #If TextLine matches phone number format
+        if textLine[3] == "-" and textLine[7] == "-":
+            dataDict["chargesByLineSummary"][dataDict["chargesByLineSummary"]["currentY"]]["phoneNumber"] = textLine.strip()
+        else:
+            if str(y) not in dataDict["chargesByLineSummary"]:
+                dataDict["chargesByLineSummary"][str(y)] = {}
+            dataDict["chargesByLineSummary"]["currentY"] = str(y)
+            dataDict["chargesByLineSummary"][str(y)]["name"] = textLine.strip()
+    elif x > 300 and x < 400:
+        if str(y) not in dataDict["chargesByLineSummary"]:
+                dataDict["chargesByLineSummary"][str(y)] = {}
+        dataDict["chargesByLineSummary"][str(y)]["amount"] = textLine.strip()
+    
+contextMap = {
+    "Charges by line summary": {
+        "final": "Total:",
+        "skip": [
+            "am a test",
+            "Smartphone"
+        ],
+        "callback": parseChargesByLineSummary
     }
 }
 
-def parse_element(eltype: str, pageNumber: int, element) -> dict:
+def parse_element(eltype: str, pageNumber: int, element, data:dict) -> dict:
     elementText = element.get_text()
     #Remove \n from element text
     elementText = elementText.replace("\n", "")
 
     if eltype == "TextLine":
-        print("pageNumber="+str(pageNumber)+",TextLine=" + elementText + ",x=" + str(round(element.x0, 2)) + ",y=" + str(round(element.y0, 2)))
-        #Loop though fields dict and find if the key is in data dict
-        #If it is, then add the text to the data dict
-        #If it is not, then ignore it
-        for key in fields:
-            field = fields[key]
-            if key not in data and field["page"] == pageNumber:
-                #Check if element text contains label
-                if field["label"] in elementText:    
-                    #Check if field contains x and y
-                    if "xy" in field:
-                        #Check if element is within x and y
-                        if element.x0 >= field["xy"]["x"] and element.y0 >= field["xy"]["y"]:
-                            data[key] = elementText
-                    else:
-                        data[key] = elementText
+        x = round(element.x0,2)
+        y = round(element.y0,2)
+        height = round(element.height,2)
+        print("TextLine=" + elementText +",Height=" + str(height) + ",x=" + str(x) + ",y=" + str(y))
+        if data['currentContext'] == None and elementText in contextMap:
+            data['currentContext'] = elementText
+            print("Context: " + data['currentContext'])
+        elif data['currentContext'] != None and elementText == contextMap[data['currentContext']]["final"]:
+            del contextMap[data['currentContext']]
+            data['currentContext'] = None
+            print("Context: None")
+        elif data['currentContext'] != None and \
+            "callback" in contextMap[data['currentContext']] \
+            and elementText not in contextMap[data['currentContext']]["skip"]:
+            contextMap[data['currentContext']]["callback"](elementText, x, y, height)
 
-    return {
-        type: eltype,
-        "xy": {
-            "x": round(element.x0, 2),
-            "y": round(element.y0, 2)
-        },
-        "subElements": []
-    }
 
-def parse_page(page: LTPage, pageNumber: int) -> list:
-    parsedPageElements = []
+def parse_page(page: LTPage, pageNumber: int, data: dict) -> list:
     for pageElement in page:
         if isinstance(pageElement, LTTextContainer):
-            parsedElement = parse_element("TextContainer", pageNumber, pageElement)
+            parse_element("TextContainer", pageNumber, pageElement, data)
             for text_line in pageElement:
                 if not isinstance(text_line, LTAnno):
-                    line = parse_element("TextLine", pageNumber, text_line)
-                    line["text"] = text_line.get_text()
-                    parsedElement["subElements"].append(line)
-                    if text_line.get_text().replace("\n", "") in skipPageText:
-                        return parsedPageElements
-            parsedPageElements.append(parsedElement)
-    return parsedPageElements
+                    parse_element("TextLine", pageNumber, text_line, data)
+                if len(contextMap) == 0:
+                    return
 
-def parse_pdf(filePath) -> list:
-    pages = []
+def parse_pdf(filePath: str, data: dict) -> list:
     pageNumber = 0
     for page_layout in extract_pages(filePath):
-        page = parse_page(page_layout, pageNumber)
-        pages.append(page)
+        parse_page(page_layout, pageNumber, data)
         pageNumber += 1
-    return pages
+        if len(contextMap) == 0:
+            return
 
-pages = parse_pdf(sys.argv[1])
+if "chargesByLineSummary" in dataDict and "currentY" in dataDict["chargesByLineSummary"]:
+    del dataDict["chargesByLineSummary"]["currentY"]
 
-print(json.dumps(pages))
+parse_pdf(sys.argv[1], dataDict)
+
+print(json.dumps(dataDict, indent=2))
