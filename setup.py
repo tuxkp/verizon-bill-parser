c78044ea6da4733acb618703f6aa20c6975d2f49
Amit <amitrke@gmail.com>
2024-09-19 14:42:58 -0700

    Added setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='verizon-bill-parser',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],
+    url='https://github.com/amitrke/verizon-bill-parser',
+    author='Amit Kumar',
+    author_email='',
+    description='A simple parser for Verizon bill PDFs',
+)
